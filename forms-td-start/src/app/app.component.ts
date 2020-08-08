6576c6c785d4a8b68c5d64d431ca842f4c801a38
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-08-08 17:25:59 -0400

    setting form values onto html using ref and setValue, method 1

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,8 +9,18 @@ export class AppComponent {
   @ViewChild('f','f') signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: this.suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });
   }
   // onSubmit(form: ElementRef)
   // onSubmit(form: NgForm){
